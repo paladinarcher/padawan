1454a043a50d0250e20bcf7a38b92d9af63548ad
Wayne Jensen <jensenw@gmail.com>
2018-05-04 13:16:26 -0600

    dash and feedback

---
+++ imports/ui/pages/dash_min/dash_min.js
@@ -0,0 +1,13 @@
+import './dash_min.html';
+
+Template.dash_min.events({
+    'click button.questions'(event, instance) {
+        FlowRouter.go('/questions');
+    },
+    'click button.learnshare'(event, instance) {
+        FlowRouter.go('/learnShareList');
+    },
+    'click button.user-segments'(event, instance) {
+        FlowRouter.go('/userSegments');
+    }
+});
