b5e5f3335c83f2f21d2560109b6d94ef710cef58
Wayne Jensen <jensenw@gmail.com>
2018-05-22 18:37:34 -0600

    verify fix

--- imports/ui/pages/verify/verify.js
+++ imports/ui/pages/verify/verify.js
@@ -1,4 +1,18 @@
+import { User } from '/imports/api/users/users.js';
 import './verify.html';
+
+Template.user_profile.onCreated(function () {
+    this.autorun( () => {
+        this.subscription = this.subscribe('userData', {
+            onStop: function () {
+                console.log("User profile subscription stopped! ", arguments, this);
+            },
+            onReady: function () {
+                console.log("User profile subscription ready! ", arguments, this);
+            }
+        });
+    });
+});
 
 Template.verify.events({
     'click #resend-verify'(event, instance) {
