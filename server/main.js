5aa7dc496ecd03ca5523d56507059cbd722f1beb
Wayne Jensen <jensenw@gmail.com>
2018-09-01 16:36:17 -0600

    login verified email fix

--- server/main.js
+++ server/main.js
@@ -2,14 +2,3 @@
 
 import '/imports/startup/both';
 import '/imports/startup/server';
-
-Accounts.validateLoginAttempt(function(options) {
-  // search through the emails, and see if it matches the email loging in with
-  if (options.methodArguments[0].user.email.verified === true) {
-      return true;
-  } else {
-      throw new Meteor.Error('email-not-verified', 'Please verify your email before logging in');
-  }
-
-
-});
