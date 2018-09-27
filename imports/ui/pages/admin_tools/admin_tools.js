2bf41f54b1ba28975a5741f749ca2910b17b91f2
Keola <keola.goodman@gmail.com>
2018-09-27 11:33:38 -0600

    fixed admin authentication on refresh| removed filter from routes added them to pages| redirected reports menu item to home

--- imports/ui/pages/admin_tools/admin_tools.js
+++ imports/ui/pages/admin_tools/admin_tools.js
@@ -1,5 +1,16 @@
 import './admin_tools.html';
 
+
+Template.admin_tools.onCreated(function () {
+    this.autorun( () => {
+        if (Roles.subscription.ready()) {
+            if (!Roles.userIsInRole(Meteor.userId(),'admin',Roles.GLOBAL_GROUP)) {
+                FlowRouter.redirect('/notfound');
+            }
+        }
+    });
+});
+
 Template.admin_tools.events({
     'click button.add_questions'(event, instance) {
         FlowRouter.go('/addQuestions/IE');
@@ -9,5 +20,8 @@ Template.admin_tools.events({
     },
     'click button.add_descriptions'(event, instance) {
         FlowRouter.go('/addTraitDescriptions');
-    }
+    },
+
+    
 });
+
