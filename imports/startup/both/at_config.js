9700670ab968766716bf1ffd631132bd9e380c52
Wayne Jensen <jensenw@gmail.com>
2018-11-27 08:00:49 -0700

    verify

--- imports/startup/both/at_config.js
+++ imports/startup/both/at_config.js
@@ -233,25 +233,6 @@ if(Meteor.isServer) {
 
         throw new Meteor.Error(403, "Not authorized to create new users");
     });
-    Accounts.validateLoginAttempt(function(attempt) {
-        if (!attempt.allowed) {
-            return false;
-        }
-
-        if ("undefined" !== typeof attempt.methodArguments[0].resume) {
-            //this isn't a new login, just resuming after page reload or similar
-            return true;
-        }
-        // search through the emails, and see if it matches the email loging in with
-        let loginEmail = attempt.user.emails.find( (element) => {
-            return element.address.toLowerCase() === attempt.methodArguments[0].user.email.toLowerCase();
-        });
-        if (loginEmail.verified) {
-            return true;
-        } else {
-            throw new Meteor.Error('email-not-verified', 'Please verify your email before logging in');
-        }
-    });
 
 	Accounts.validateLoginAttempt(function(attempt) {
 		if (!attempt.allowed) {
