4bfaa692d02a0bf1e389603238380434418213b2
Tina Orozco <tmorozco1990@gmail.com>
2018-11-28 10:25:43 -0700

    add admin_reports html & js files

---
+++ imports/ui/pages/admin_reports/admin_reports.js
@@ -0,0 +1,30 @@
+import './admin_reports.html'
+import { Template } from 'meteor/templating'
+import { Reports, Report } from '../../api/reports/reports.js'
+import { Meteor } from 'meteor/meteor'
+
+// onCreated
+Template.admin_reports.onCreated(function () {
+    this.autorun(() => {
+        console.log(`subscribe to 'reports' db...`)
+        this.subscription = this.subscribe('reports', {
+            onStop: function () {
+                console.log("Subscription stopped! ", arguments, this);
+            },
+            onReady: function () {
+                console.log("Subscription ready! ", arguments, this);
+            }
+        })
+    })
+})
+
+
+// helpers
+Template.admin_reports.helpers({
+
+})
+
+// events
+Template.admin_reports.events({
+
+})
