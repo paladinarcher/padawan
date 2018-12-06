6c2f6ed87630cf9080dbfeb96723ede584cd0014
Tina Orozco <tmorozco1990@gmail.com>
2018-12-06 12:37:35 -0700

    Refactor admin_reports.js

--- imports/ui/pages/admin_reports/admin_reports.js
+++ imports/ui/pages/admin_reports/admin_reports.js
@@ -5,29 +5,65 @@ import {Report, Reports} from '/imports/api/reports/reports.js'
 import {FlowRouter} from 'meteor/kadira:flow-router'
 import {ReactiveVar} from 'meteor/reactive-var'
 
-// onCreated
-Template.admin_reports.onCreated(function () {
-    this.autorun(() => {
-        // subscribe to the reports db
-        this.subscription = this.subscribe('reports', {
-            onStop: function () {
-                console.log(`MESSAGE:  Subscription stopped!`);
-                console.log(`ARGS:  ${arguments}`);
-                console.log(`THIS:  ${this}`);
-            },
-            onReady: function () {
-                // console.log("Subscription ready! ", arguments, this);
-                console.log(`MESSAGE:  Subscription Ready!`);
-                console.log(`ARGS:  ${arguments}`);
-                console.log(`THIS:  ${this}`);
-            }
-        })
+
+/**
+ * Functions
+ */
+
+/**
+ * converts an object into an array of name and value objects for iterability on the client side
+ * ref: https://stackoverflow.com/questions/15035363/meteor-and-handlebars-each-to-iterate-over-object
+ * @param  {Object} obj     object to be converted into an array of name, value objects
+ * @return {Array}  result  an array of objects with {name, value} params
+ */
+function arrayify (obj) {
+    var result = []
+    for (var key in obj) result.push({name:key,value:obj[key]})
+    return result
+}
+
+/**
+ * displays console messages on sub stop
+ * @return {Boolean} returns false on completion
+ */
+function stopMsgs() {
+    console.log(`MESSAGE:  Subscription stopped!`);
+    console.log(`ARGS:  ${arguments}`);
+    console.log(`THIS:  ${this}`);
+    return false
+}
+
+/**
+ * displays console messages on sub ready
+ * @return {Boolean} returns false on completion
+ */
+function readyMsgs() {
+    console.log(`MESSAGE:  Subscription Ready!`);
+    console.log(`ARGS:  ${arguments}`);
+    console.log(`THIS:  ${this}`);
+    return false
+}
+
+/**
+ * subs the template to the reports publication
+ * @param   {Object}  self   Template instance (referred to below as 'this')
+ * @return  {Boolean}        Return false by default (no other value needs to be returned)
+ */
+function reportSub(self) {
+    // subscribe to the reports db
+    self.subscription = self.subscribe('reports', {
+        onStop: stopMsgs(),
+        onReady: readyMsgs()
     })
-})
+    return false
+}
 
 
-// helpers
-Template.admin_reports.helpers({
+/**
+ * Helpers and Events
+ */
+
+const adminReportsTempHelpers = {
     // return all the reports from the reports db
     reports() {
         console.log('Reports: ', Reports.find({}))
@@ -45,10 +81,9 @@ Template.admin_reports.helpers({
         console.log(typeof(rDateString))
         console.log(rDateString)
     }
-})
+}
 
-// events
-Template.admin_reports.events({
+const adminReportsTempEvents = {
     'click a.report-title' (event, instance) {
         // grab report id for url
         console.log(event.target.dataset.loc)
@@ -57,55 +92,9 @@ Template.admin_reports.events({
         // navigate to the report by id
         FlowRouter.go(`/tools/reports/${reportLocation}`)
     }
-})
+}
 
-
-
-// autorun for default report template
-Template.report_default.onCreated(function () {
-    this.autorun(() => {
-        // subscribe to the reports db
-        this.subscription = this.subscribe('reports', {
-            onStop: function () {
-                console.log(`MESSAGE:  Subscription stopped!`);
-                console.log(`ARGS:  ${arguments}`);
-                console.log(`THIS:  ${this}`);
-            },
-            onReady: function () {
-                // console.log("Subscription ready! ", arguments, this);
-                console.log(`MESSAGE:  Subscription Ready!`);
-                console.log(`ARGS:  ${arguments}`);
-                console.log(`THIS:  ${this}`);
-            }
-        })
-        this.reportId = FlowRouter.getParam("_id")
-        this.getReport = (reportId) => Reports.findOne({_id:reportId})
-        this.report = this.getReport(this.reportId)
-        // console.log(this.reportId)
-        // console.log(this.report)
-    })
-})
-
-// // onCreated functions
-// Template.report_default.onCreated(function defaultReportOnCreated(){
-//     this.reportId = FlowRouter.getParam("_id")
-//     this.getReport = (reportId) => Reports.findOne({_id:reportId})
-//     this.report = this.getReport(this.reportId)
-//     // console.log(this.reportId)
-//     // console.log(this.report)
-// })
-
-// arrayify obj for use on the client side
-// ref: https://stackoverflow.com/questions/15035363/meteor-and-handlebars-each-to-iterate-over-object
-Template.registerHelper('arrayify',function(obj){
-    var result = [];
-    for (var key in obj) result.push({name:key,value:obj[key]});
-    return result;
-});
-
-
-// default report template helpers
-Template.report_default.helpers({
+const reportDefaultTempHelpers = {
     reportTitle () {
         const r = Template.instance().report
         const rTitle = r.title;
@@ -114,13 +103,62 @@ Template.report_default.helpers({
     reportDate () {
         const r = Template.instance().report
         const rDate = r.dateCreated;
-        const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }
-
         return rDate
     },
     reportData () {
         const r = Template.instance().report
         const rData = r.data
         return r.data.reportData
+    },
+    ifObject (val) {
+        console.log(val.hash.val)
+        console.log(val)
+        // the value is an object, return true
+        if (typeof(val.hash.val) == 'object') {
+            return true
+        }
+        // The value is not an object, return false
+        else {
+            return false
+        }
+        // for some reason the if statement wasn't hit. Return false as a default
+        return false
+    },
+    convertObj (val) {
+        return JSON.stringify(val.hash.val)
     }
-});
+}
+
+/**
+ * Main
+ */
+
+// register arrayify as a global template helper
+Template.registerHelper('arrayify', arrayify)
+
+
+// onCreated actions for admin report template
+Template.admin_reports.onCreated(function reportSubs() {
+    this.autorun(() => reportSub(this))
+})
+// set  admin_report helpers
+Template.admin_reports.helpers(adminReportsTempHelpers)
+// set admin_report temp events
+Template.admin_reports.events(adminReportsTempEvents)
+
+
+// default reports onCreated actions
+Template.report_default.onCreated(function () {
+    this.autorun(() => {
+        // sub to reports
+        reportSub(this)
+        // set flowrouter id param to the report selected
+        this.reportId = FlowRouter.getParam("_id")
+        // get the report selected from the db
+        this.getReport = (reportId) => Reports.findOne({_id:reportId})
+        // store selected report as param
+        this.report = this.getReport(this.reportId)
+    })
+})
+// set default report template helpers
+Template.report_default.helpers(reportDefaultTempHelpers)
