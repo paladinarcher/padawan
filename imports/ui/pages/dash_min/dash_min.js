195d2819ecc85a8c7a059cb7dcd556d3437fe80b
Carl Shurtliff <cphilshurtliff@gmail.com>
2018-12-07 16:25:03 -0700

    qnaire/assessment display is off to a good skeleton start

--- imports/ui/pages/dash_min/dash_min.js
+++ imports/ui/pages/dash_min/dash_min.js
@@ -1,3 +1,4 @@
+import { Qnaire } from '../qnaire/qnaire.js';
 import './dash_min.html';
 
 Template.dash_min.events({
@@ -10,4 +11,17 @@ Template.dash_min.events({
     'click button.user-segments'(event, instance) {
         FlowRouter.go('/userSegments');
     }
+});
+
+
+Template.dash_min.helpers({
+	assessments() {
+		//return Qnaire.find().count();
+		return [1, 2, 3, 4];
+	},
+    getAssessment() { //console.log(this.index, arguments, this);
+		return "Hello assessment";
+        //return (this.index % 2) ? Template.questionTemplate : Template.questionTemplateReversed;
+    },
+
 });
