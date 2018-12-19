e8fb1f0d642dba5939f95c92b2c202ac5e37fcef
Tina Orozco <tmorozco1990@gmail.com>
2018-12-19 11:51:33 -0700

    get user data to display on mbti report

--- imports/api/reports/customReports.js
+++ imports/api/reports/customReports.js
@@ -1,84 +1,33 @@
 import { Report, Reports } from "./reports.js";
-import { Users } from "../users/users.js";
-
-/**
- * @name createMBTIReport
- * @description builds the MBTI Report
- * @param {*} params 
- */
-function createMBTIReport(params) {
-
-    let mbtiReport = new Report()
-    
-    //TODO: get report data here 
-    mbtiReport.reportData = {
-        all: []
-    }
-
-    let allUserData = mbtiReport.reportData.all
-
-    // test data 
-    let testData = {
-        userName: 'Test Person',
-        personality: {
-            IE: {
-                values: 0,
-                totals: 0,
-                questionCount: 0
-            },
-            NS: {
-                values: 0,
-                totals: 0,
-                questionCount: 0
-            },
-            TF: {
-                values: 0,
-                totals: 0,
-                questionCount: 0
-            },
-            JP: {
-                values: 0,
-                totals: 0,
-                questionCount: 0
-            }
-        }
-    }
-
-    // test data 
-    let testData2 = {
-        userName: 'Test Person 2',
-        personality: {
-            IE: {
-                values: 0,
-                totals: 0,
-                questionCount: 0
-            },
-            NS: {
-                values: 0,
-                totals: 0,
-                questionCount: 0
-            },
-            TF: {
-                values: 0,
-                totals: 0,
-                questionCount: 0
-            },
-            JP: {
-                values: 0,
-                totals: 0,
-                questionCount: 0
-            }
-        }
-    }
-
-    // push the data to the array in the report 
-    allUserData.push(testData)
-    allUserData.push(testData2)
-
-    return mbtiReport;
-}
+import { User, Users } from "../users/users.js";
 
 function addMBTIReport () {
+
+    /**
+     * @name createMBTIReport
+     * @description builds the MBTI Report
+     * @param {*} params 
+     */
+    function createMBTIReport(params) {
+
+        let u = User.find().fetch();
+        let mbtiReport = new Report()
+
+        mbtiReport.reportData = {
+            all: []
+        }
+
+        let allUserData = mbtiReport.reportData.all
+
+        u.forEach((m) => {
+            allUserData.push({
+                userName: m.MyProfile.firstName + ' ' + m.MyProfile.lastName,
+                personality: m.MyProfile.UserType.Personality,
+            })
+        })
+
+        return mbtiReport;
+    }
 
     let newMBTIReport = new Reports({
         title: 'mbti',
