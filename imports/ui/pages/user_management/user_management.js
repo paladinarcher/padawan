5d2aafc632a16c45f2b46d35dbfe97db166dce20
Tina Orozco <tmorozco1990@gmail.com>
2018-12-20 11:16:40 -0700

    create template and onCreated fn for user_management

---
+++ imports/ui/pages/user_management/user_management.js
@@ -0,0 +1,10 @@
+import "./user_management.html";
+Template.user_management.onCreated(function () {
+    this.autorun(() => {
+        if (Roles.subscription.ready()) {
+            if (!Roles.userIsInRole(Meteor.userId(), 'admin', Roles.GLOBAL_GROUP)) {
+                FlowRouter.redirect('/notfound');
+            }
+        }
+    });
+});
