1194f36a6a389dd7c07e30647eae8c1c14757b6c
Tina Orozco <tmorozco1990@gmail.com>
2018-12-21 11:43:09 -0700

    subscribe to users db

--- imports/ui/pages/user_management/user_management.js
+++ imports/ui/pages/user_management/user_management.js
@@ -1,16 +1,51 @@
 import "./user_management.html";
+import { User } from "../../../api/users/users.js";
 
 
 const colors = [
     'primary', 'info', 'warning', 'default',
 ]
 
+const helpers = {
+    allUsers(){
+        console.log(this.allUsers)
+    },
+    currentlyAssignedRoles(){},
+    pickRandomRoleColor(colors, userCurrentRoles){}
+    // addRoleToUser(){}, 
+    // removeRoleFromUser(){},
+}
 
-const helpers = {}
+const events = {
+    'click .um-add-role': function addRoleToUser(event, instance) {
+        // add role function 
+    },
+    'click .um-remove-role': function removeRoleFromUser(event, instance) {
+        // remove role function 
+    }
+}
 
-const events = {}
+
+// Functions
+function subOnReady() {
+    console.log('Subscription is ready!')
+}
+
+function subOnStop() {
+    console.log('Subscription Stopped!')
+}
+
+function subscribeToUsers(self) {
+    self.subscription = self.subscribe('users', {
+        onReady: subOnReady(),
+        onStop: subOnStop()
+    })
+    console.log('subscribed to users DB')
+    return false
+}
 
 
+// main
 Template.user_management.onCreated(function () {
     this.autorun(() => {
         if (Roles.subscription.ready()) {
@@ -18,7 +53,11 @@ Template.user_management.onCreated(function () {
                 FlowRouter.redirect('/notfound');
             }
         }
+        subscribeToUsers(this)
+        this.allUsers = User.find({}).fetch();
+        console.log(this.allUsers)
     });
+
 });
 
 Template.user_management.helpers(helpers);
