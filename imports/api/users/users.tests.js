c38d2b8aea2471af98b59b9f39fb8149eacd74c7
Tina Orozco <tmorozco1990@gmail.com>
2019-01-07 14:11:41 -0700

    create test data

--- imports/api/users/users.tests.js
+++ imports/api/users/users.tests.js
@@ -3,6 +3,48 @@ import { chai } from 'meteor/practicalmeteor:chai';
 import { User } from './users.js';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 
+FactoryBoy.define("nonAdminUser", User, {
+    _id: "1234567899912839",
+    services: {
+        password: {}
+    },
+    username: "bestTestUser",
+    emails: [],
+    slug: "testUser@domain.com",
+    MyProfile: {
+        firstName: "testUser",
+        lastName: "test",
+        gender: true,
+        UserType: {
+            Personality: {},
+            AnsweredQuestions: []
+        },
+        birthDate: new Date("December 17, 1995 03:24:00")
+    },
+    teams: [],
+    roles: {},
+    profile: {
+        first_name: "testUser",
+        last_name: "test",
+        gender: "male"
+    }
+});
+
+FactoryBoy.define("adminUser1", User, {
+    _id: "999",
+    services: {
+        password: {}
+    },
+    MyProfile: {
+        firstName: "adminUser",
+        lastName: "admin",
+        gender: true
+    },
+    roles: {
+        __global_roles__: ["admin"]
+    }
+});
+
 if (Meteor.isServer) {
     describe('User', function () {
         this.timeout(15000)
