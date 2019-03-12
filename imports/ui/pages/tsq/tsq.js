2b8fd11311ebc1924678b4dedb569e4a6352d00d
Tina Colvin <tmorozco1990@gmail.com>
2019-03-12 15:19:43 -0600

    work on getusers and add users functions

--- imports/ui/pages/tsq/tsq.js
+++ imports/ui/pages/tsq/tsq.js
@@ -17,17 +17,17 @@ let testData = {
 // TODO: change to config file
 const USER_URL = 'http://localhost:4000/tsq/skills/users/'
 
-// return true if the user has a TSQ Key
-let hasTSQData = new ReactiveVar(true)
-
 // return false if the user does not have any skills data
 let alreadyHasSkills = new ReactiveVar(false)
+let userSkillsEntered = new ReactiveVar()
 
 // variable to hold the current user data
 let user;
 
 // var to hold user technicalSkillsData
 let userKey;
+
+let skills; 
 
 /**
  * Functions
@@ -54,13 +54,32 @@ async function registerKey (url, user) {
 	return
 }
 
-async function getUsersSkills (url, userKey) {
-	let options = { method: "GET" }
-	let response = await fetch(url + '/findOne/key/' + userKey, options)
-	let json = await response.json()
+async function getUserSkills (url, userKey, skillvariable) {
+	const options = { method: "GET" }
+	const response = await fetch(url + '/findOne/key/' + userKey, options)
+	console.log(response)
+	const json = await response.json()
 	console.log(json)
-	console.log(json.data.payload.skills)
+	skillvariable = json.data.payload.skills
+	// return json.data.payload.skills
 }
+
+/**
+* @param url - URL 
+* @param userKey - the user's tsq key 
+* @param skills - Array of skills 
+*/
+async function addUserSkills (url, userKey, skills) { 
+	let options = { 
+		method: "PUT",  
+		body: { "skills": skills }
+	}
+	let response = await fetch(url + 'addSkills/key/' + userKey, options)
+	let json = await response.json()
+	console.log('json' + json)
+	
+}
+
 
 /**
  * Templates
@@ -85,11 +104,15 @@ Template.tsq_userLanguageList.onCreated(function () {
 				// check if user doesnt have key, register one if not
 				if(userKey === undefined){
 					registerKey(USER_URL, user);
-					getUsersSkills(USER_URL, userKey)
+					getUserSkills(USER_URL, userKey)
+					addUserSkills(USER_URL, userKey, [{ name: 'javascript' }])
+					console.log(getUserSkills(USER_URL, userKey))
 				}else{
 					console.log("user already has key stored!", userKey);
-					getUsersSkills(USER_URL, userKey)
+					console.log(getUserSkills(USER_URL, userKey, skills))
 				};
+
+				console.log('Skills: ' + skills)
 			}
 		});
 	})
@@ -118,7 +141,17 @@ Template.tsq_pasteProfile.events({
 	'click .tsq-enterSkillsContinue': function (event, instance) {
 		let userEnteredText = $('#tsq-enterSkillsTextarea').val().toString().trim()
 		userSkillsEntered.set(userEnteredText.split(','))
-		console.log(userSkillsEntered)
+		console.log('USER Skills Entered: ' + userSkillsEntered.get())
+		
+		// TODO: Search tsq skills db for items in the userSkillsEntered array
+		fetch('http://localhost:4000/tsq/skills/')
+			.then(response => response.json())
+			.then(jsonData => {
+				console.log(jsonData)
+			})
+		// TODO: if not exist in skills db add to skills db 
+		// TODO: if not already added to user, add to user 
+
 		return
 	}
 });
