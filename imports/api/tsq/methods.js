b6dc1588aee83d55367ec5e735d6c96ae1760bbf
Tina Colvin <tmorozco1990@gmail.com>
2019-03-13 16:56:36 -0600

    change register key call to a meteor method
    - registering a key to a user is done through a meteor method now

--- imports/api/tsq/methods.js
+++ imports/api/tsq/methods.js
@@ -2,14 +2,24 @@ import { Meteor } from 'meteor/meteor';
 import { HTTP } from 'meteor/http';
 
 Meteor.methods({
-    'tsq.getKeyData' (key) {
-        let modifiedResult;
-        try {
-          let result = HTTP.get('http://localhost:4000/tsq/skills/users/findOne/key/' + key);
-          modifiedResult = result;
-        } catch (e) {
-          throw new Meteor.Error('some-error-code', 'Something bad went down');
-        }
-        return modifiedResult;
-    },
+  'tsq.registerKeyToUser' () {
+    let modifiedResult;
+    try {
+      let result = HTTP.post('http://localhost:4000/tsq/skills/users/register');
+      modifiedResult = result;
+    } catch (e) {
+      throw new Meteor.Error('some-error-code', 'Something bad went down');
+    }
+    return modifiedResult;
+  },  
+  'tsq.getKeyData' (key) {
+      let modifiedResult;
+      try {
+        let result = HTTP.get('http://localhost:4000/tsq/skills/users/findOne/key/' + key);
+        modifiedResult = result;
+      } catch (e) {
+        throw new Meteor.Error('some-error-code', 'Something bad went down');
+      }
+      return modifiedResult;
+  },
 })
