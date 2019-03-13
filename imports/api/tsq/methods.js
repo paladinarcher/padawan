6ae1d605497d0ccaa20251f29862933beb1eef74
Tina Colvin <tmorozco1990@gmail.com>
2019-03-13 16:29:15 -0600

    add http pkg. query api data

---
+++ imports/api/tsq/methods.js
@@ -0,0 +1,15 @@
+import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
+
+Meteor.methods({
+    'tsq.getKeyData' (key) {
+        let modifiedResult;
+        try {
+          let result = HTTP.get('http://localhost:4000/tsq/skills/users/findOne/key/' + key);
+          modifiedResult = result;
+        } catch (e) {
+          throw new Meteor.Error('some-error-code', 'Something bad went down');
+        }
+        return modifiedResult;
+    },
+})
--- imports/ui/pages/tsq/tsq.js
+++ imports/ui/pages/tsq/tsq.js
@@ -18,15 +18,16 @@ let testData = {
 const USER_URL = 'http://localhost:4000/tsq/skills/users/'
 
 // return false if the user does not have any skills data
-let alreadyHasSkills = new ReactiveVar(checkUserSkills())
+// let alreadyHasSkills = new ReactiveVar(checkUserSkills())
 let userSkillsEntered = new ReactiveVar()
 
 // variable to hold the current user data
 let user;
 
 // var to hold user technicalSkillsData
-let userKey;
-let skills;
+// let userKey;
+// let skills;
+
 /**
  * Functions
  */
@@ -52,85 +53,6 @@ async function registerKey (url, user) {
 	return
 }
 
-// async function getUserSkills (url, userKey, skillvariable) {
-// 	const options = { method: "GET" }
-// 	const response = await fetch(url + '/findOne/key/' + userKey, options)
-// 	console.log(response)
-// 	const json = await response.json()
-// 	console.log(json)
-// 	skillvariable = json.data.payload.skills
-// 	// return json.data.payload.skills
-let myData = {}
-async function getUsersSkills (url, userKey) {
-	let options = { method: "GET" }
-
-	return fetch(url + '/findOne/key/' + userKey, options)
-		.then(results => {
-			// let response = results.clone().json();
-			return results.json();
-		}).then(data => {
-			// console.log('data right here: ', data.data.payload);
-			myData = data;
-			console.log('myData here: ', myData);
-			// return myData;
-			// return data;
-
-		})
-
-	// let response = await fetch(url + '/findOne/key/' + userKey, options)
-	// console.log('get response: ', response);
-	// let json = await response.json()
-	// console.log("json: ", json)
-	// console.log("payload skills: ", json.data.payload.skills)
-	// skills = json.map(e => e.data.payload);
-	// console.log('first skills: ', skills);
-}
-
-function checkUserSkills (myData) {
-	console.log('myData in check user: ', myData);
-	// if(myData !== {}){
-	// 	if(myData.data.payload.skills === []){
-	// 		return false
-	// 	} else {
-	// 		return true
-	// 	}
-	// }
-	// return false
-}
-
-/**
-* @param url - URL 
-* @param userKey - the user's tsq key 
-* @param skills - Array of skills 
-*/
-async function addUserSkills (url, userKey, skills) { 
-	console.log('do we even make it here: ', skills);
-	let options = { 
-		method: "PUT",  
-		body: { "skills": skills }
-	}
-	let response = await fetch(url + 'addSkills/key/' + userKey, options)
-	console.log('add response: ', response);
-	// let json = await response.json()
-	// console.log('add JSON: ' + json)
-}
-
-// /**
-// * @param url - URL 
-// * @param userKey - the user's tsq key 
-// * @param skills - Array of skills 
-// */
-// async function addUserSkills (url, userKey, skills) { 
-// 	let options = { 
-// 		method: "PUT",  
-// 		body: { "skills": skills }
-// 	}
-// 	let response = await fetch(url + 'addSkills/key/' + userKey, options)
-// 	let json = await response.json()
-// 	console.log('json' + json)
-	
-// }
-
 
 /**
  * Templates
@@ -139,7 +61,7 @@ async function addUserSkills (url, userKey, skills) {
 // main temp
 Template.tsq_userLanguageList.onCreated(function () {
 	this.autorun(() => {
-		this.subscription = this.subscribe('tsqUserList', this.userId, {
+		this.subscription1 = this.subscribe('tsqUserList', this.userId, {
 			onStop: function () {
 				console.log("tsq user List subscription stopped! ", arguments, this);
 			},
@@ -150,27 +72,18 @@ Template.tsq_userLanguageList.onCreated(function () {
 				console.log('user id: ', userId);
 				user = User.findOne({_id:userId});
 				console.log('user: ', user);
-				userKey = user.MyProfile.technicalSkillsData;
+				// userkey = user.MyProfile.technicalSkillsData
+				// console.log('this.userkey(154): ' + userkey)
 
-				// addUserSkills(USER_URL, userKey, "hello")
-
-				// getUsersSkills(USER_URL, userKey).then(e => console.log('get being called: ', e.data.payload.skills))
-				getUsersSkills(USER_URL, userKey);
-				// console.log('myData here also?: ', myData);
-
-				// console.log('skills right here?: ', skills);
-				// console.log('this is myData right here: ', myData);
-
-				// check if user doesnt have key, register one if not
-				if(userKey === undefined){
+				if(user.MyProfile.technicalSkillsData === undefined){
 					registerKey(USER_URL, user);
-					// addUserSkills(USER_URL, userKey, [{ name: 'javascript' }])
-					// console.log(getUserSkills(USER_URL, userKey))
 				}else{
-					console.log("user already has key stored!", userKey);
+					console.log("user already has key stored!", user.MyProfile.technicalSkillsData);
 				};
 
-				// console.log('Skills: ' + skills)
+				Meteor.call('tsq.getKeyData', user.MyProfile.technicalSkillsData, (error, result) => {
+					console.log(result)
+				});
 			}
 		});
 	})
@@ -182,7 +95,7 @@ Template.tsq_userLanguageList.helpers({
 })
 
 Template.tsq_userLanguageList.rendered = function(){
-	console.log('does it make it here: ', myData);
+	// console.log('does it make it here: ', myData);
 }
 
 // enter skill textarea and next button
@@ -193,8 +106,8 @@ Template.tsq_pasteProfile.rendered = function () {
 
 Template.tsq_pasteProfile.helpers({
 	alreadyHasSkills () {
-		return alreadyHasSkills.get()
-
+		// return alreadyHasSkills.get()
+		return false 
 	},
 })
 
@@ -204,17 +117,9 @@ Template.tsq_pasteProfile.events({
 		let userEnteredText = $('#tsq-enterSkillsTextarea').val().toString().trim()
 		userSkillsEntered.set(userEnteredText.split(','))
 		console.log('USER Skills Entered: ' + userSkillsEntered.get())
-		console.log('myData here also?: ', myData.data.payload.skills);
-		
 		// TODO: Search tsq skills db for items in the userSkillsEntered array
-		fetch('http://localhost:4000/tsq/skills/')
-			.then(response => response.json())
-			.then(jsonData => {
-				console.log(jsonData)
-			})
 		// TODO: if not exist in skills db add to skills db 
 		// TODO: if not already added to user, add to user 
-
 		return
 	}
 });
