78c51c8aec1c9391a2f1837f392476b93d4515e1
Tina Colvin <tmorozco1990@gmail.com>
2019-04-15 17:52:37 -0600

    fix: mocha tests not working
    - tests failing due to validation error on technicalSkillsData field in the profile class of the user

--- imports/api/questions/questions.test.js
+++ imports/api/questions/questions.test.js
@@ -5,182 +5,189 @@ import { PolarStats } from './questions.js';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 import { Defaults } from '/imports/startup/both/defaults.js';
 import { User } from '../users/users.js';
-import { Answer } from "../users/users.js";
-import { UserType } from "../users/users.js";
-import { Profile } from "../users/users.js";
+import { Answer } from '../users/users.js';
+import { UserType } from '../users/users.js';
+import { Profile } from '../users/users.js';
 import { UserNotify } from '/imports/api/user_notify/user_notify.js';
 import '/imports/startup/both/at_config.js';
-import { Random } from 'meteor/random'
+import { Random } from 'meteor/random';
 import { Team } from '../teams/teams.js';
-
 
 let testData;
 let defaultUser;
 
-
-
-FactoryBoy.define("user2", User, {
-  _id: "12345678942",
+FactoryBoy.define('user2', User, {
+  _id: '12345678942',
   services: {
     password: {}
   },
-  username: "TestUser42",
+  username: 'TestUser42',
   emails: [],
-  slug: "testUser42@domain.com",
+  slug: 'testUser42@domain.com',
   MyProfile: {
-    firstName: "testUser42",
-    lastName: "test42",
+    firstName: 'testUser42',
+    lastName: 'test42',
     gender: true,
     UserType: {
       Personality: {},
       AnsweredQuestions: []
     },
-    birthDate: new Date("December 22, 1995 03:24:00")
+    birthDate: new Date('December 22, 1995 03:24:00'),
+    technicalSkillsData: 'th3TestK3y'
   },
   teams: [],
   roles: {},
   profile: {
-    first_name: "testUser42",
-    last_name: "test42",
-    gender: "male"
+    first_name: 'testUser42',
+    last_name: 'test42',
+    gender: 'male'
   }
 });
 
-FactoryBoy.define("question1", Question, {
-	_id: '98765432142',
-	Category: 0,
-//	Categories: [],
-//	Text: 'Text Unit Test',
-//	LeftText: 'Unit Test LeftText',
-//	RightText: 'Unit Test RightText',
-//	Readings: [],
-//	segments: [],
-//	active: false,
-	CreatedBy: 'Bill Murray',
-	mochaTesting: true
+FactoryBoy.define('question1', Question, {
+  _id: '98765432142',
+  Category: 0,
+  //	Categories: [],
+  //	Text: 'Text Unit Test',
+  //	LeftText: 'Unit Test LeftText',
+  //	RightText: 'Unit Test RightText',
+  //	Readings: [],
+  //	segments: [],
+  //	active: false,
+  CreatedBy: 'Bill Murray',
+  mochaTesting: true
 });
 
-FactoryBoy.define("answer1", Answer, {
-	QuestionID: '98765432142'
+FactoryBoy.define('answer1', Answer, {
+  QuestionID: '98765432142'
 });
 
-if (Meteor.isServer)
-{
-    describe('Questions', function () {
-        this.timeout(15000);
+if (Meteor.isServer) {
+  describe('Questions', function() {
+    this.timeout(15000);
 
-        beforeEach(function() {
-            resetDatabase();
+    beforeEach(function() {
+      resetDatabase();
 
-			
-//			if(Meteor.users.find().count() < 1) {
-//				defaultUser = Accounts.createUser({
-//					username: Defaults.user.username,
-//					email: Defaults.user.email,
-//					password: "password",
-//					isAdmin: Defaults.user.isAdmin,
-//					profile: Defaults.user.profile,
-//					teams: [Team.Default.Name]
-//				});
-//				//let t = Team.findOne( {Name: Team.Default.Name} );
-//				//t.CreatedBy = defaultUser._id;
-//				//t.save();
-//			}
-//
-//
-//
-//            testData = {
-//                testQuestion: {
-//                    Text: 'Text Unit Test',
-//                    LeftText: 'Unit Test LeftText',
-//                    RightText: 'Unit Test RightText',
-//                    CreatedBy: 'TestId', //defaultUser._id.str,
-//                    Category: 0,
-//					//_id: 'TestQuestionId'
-//                }
-//            };
-//
-//            //sinon.stub(Meteor, 'userId').returns(defaultUser._id);
-			
-        });
-
-        afterEach(function () {
-            //Meteor.userId.restore();
-            resetDatabase();
-        });
-        it('totalQuestions is greater than or equal to 0', function () {
-            let totalQuestions = Question.find().count();
-            chai.assert(totalQuestions >= 0, 'totalQuestions is negative');
-        });
-
-        it('can create a question', function () {
-			let user42 = FactoryBoy.create('user2');
-			let findUser = User.findOne({_id: user42._id});
-            let q = FactoryBoy.create('question1');
-			let findQuestion = Question.findOne({_id: "98765432142"});
-            let qTest = Question.find( {_id: q._id} );
-			chai.assert(qTest != undefined);
-        });
-
-        // Test of Question methods and helpers
-        it('getUser returns the user that created the question', function() {
-            let u = FactoryBoy.create('user2');
-            let q = FactoryBoy.create('question1', {CreatedBy: u._id});
-            let qTest = q.getUser();
-            chai.assert.equal( qTest._id, u._id );
-
-        });
-        it('Negative addAnswer results in addition to TimesAnswered.LeftSum', function() {
-            let q = FactoryBoy.create('question1');
-            let lsStart = q.TimesAnswered.LeftSum;
-            let ans = {Value: -1};
-            q.addAnswer(ans);
-            let lsFinal = q.TimesAnswered.LeftSum;
-            chai.assert(lsFinal == lsStart + 1);
-
-        });
-        it('Positve removeAnswer results in negation of TimesAnswered.RightSum', function() {
-            let q = FactoryBoy.create('question1');
-            let rsStart = q.TimesAnswered.RightSum;
-            let ans = {Value: 1};
-            q.addAnswer(ans);
-            let rsAdd = q.TimesAnswered.RightSum;
-            chai.assert(rsAdd == rsStart + 1);
-            q.removeAnswer(ans);
-            let rsRemove = q.TimesAnswered.RightSum;
-            chai.assert(rsRemove == rsAdd - 1);
-        });
-
-        it('allAnsweredUsers returns at least one user that answered the question', function() {
-            let q = FactoryBoy.create('question1');
-            let u = FactoryBoy.create('user2');
-            let a = FactoryBoy.build('answer1');
-			//u.MyProfile.UserType.answerQuestion(q);
-            let myStub = sinon.stub(Meteor, 'userId').returns(u);
-			User.update({_id: u._id}, {$push: {'MyProfile.UserType.AnsweredQuestions': a}});
-            myStub.restore();
-
-			let u2 = User.find({_id: u._id}).fetch()
-			//console.log(u2[0].MyProfile.UserType.AnsweredQuestions[0].QuestionID);
-			chai.assert.equal(u2[0].MyProfile.UserType.AnsweredQuestions[0].QuestionID, q._id, "Answer ID is not the same as Question ID");
-			let ansUser = q.allAnsweredUsers();
-			//console.log(ansUser.fetch());
-            chai.assert.equal( ansUser.fetch()[0]._id, u._id, "question.allAnsweredUsers doesn't match the user ID");
-		});
-        it('reset() resets TimesAnswered and SumOfAnswers', function() {
-            let q = FactoryBoy.create('question1');
-            let ans = {Value: -2};
-            q.addAnswer(ans);
-            let taStart = q.TimesAnswered.LeftSum;
-            let soaStart = q.SumOfAnswers.LeftSum;
-            chai.assert((taStart == 1) && (soaStart == -2), "Error while adding an answer");
-            q.reset();
-            let taFinish = q.TimesAnswered.LeftSum;
-            let soaFinish = q.SumOfAnswers.LeftSum;
-            chai.assert((taFinish == 0) && (soaFinish == 0), "Error while reseting answers");
-        });
-
-
+      //			if(Meteor.users.find().count() < 1) {
+      //				defaultUser = Accounts.createUser({
+      //					username: Defaults.user.username,
+      //					email: Defaults.user.email,
+      //					password: "password",
+      //					isAdmin: Defaults.user.isAdmin,
+      //					profile: Defaults.user.profile,
+      //					teams: [Team.Default.Name]
+      //				});
+      //				//let t = Team.findOne( {Name: Team.Default.Name} );
+      //				//t.CreatedBy = defaultUser._id;
+      //				//t.save();
+      //			}
+      //
+      //
+      //
+      //            testData = {
+      //                testQuestion: {
+      //                    Text: 'Text Unit Test',
+      //                    LeftText: 'Unit Test LeftText',
+      //                    RightText: 'Unit Test RightText',
+      //                    CreatedBy: 'TestId', //defaultUser._id.str,
+      //                    Category: 0,
+      //					//_id: 'TestQuestionId'
+      //                }
+      //            };
+      //
+      //            //sinon.stub(Meteor, 'userId').returns(defaultUser._id);
     });
-}
 
+    afterEach(function() {
+      //Meteor.userId.restore();
+      resetDatabase();
+    });
+    it('totalQuestions is greater than or equal to 0', function() {
+      let totalQuestions = Question.find().count();
+      chai.assert(totalQuestions >= 0, 'totalQuestions is negative');
+    });
+
+    it('can create a question', function() {
+      let user42 = FactoryBoy.create('user2');
+      let findUser = User.findOne({ _id: user42._id });
+      let q = FactoryBoy.create('question1');
+      let findQuestion = Question.findOne({ _id: '98765432142' });
+      let qTest = Question.find({ _id: q._id });
+      chai.assert(qTest != undefined);
+    });
+
+    // Test of Question methods and helpers
+    it('getUser returns the user that created the question', function() {
+      let u = FactoryBoy.create('user2');
+      let q = FactoryBoy.create('question1', { CreatedBy: u._id });
+      let qTest = q.getUser();
+      chai.assert.equal(qTest._id, u._id);
+    });
+    it('Negative addAnswer results in addition to TimesAnswered.LeftSum', function() {
+      let q = FactoryBoy.create('question1');
+      let lsStart = q.TimesAnswered.LeftSum;
+      let ans = { Value: -1 };
+      q.addAnswer(ans);
+      let lsFinal = q.TimesAnswered.LeftSum;
+      chai.assert(lsFinal == lsStart + 1);
+    });
+    it('Positve removeAnswer results in negation of TimesAnswered.RightSum', function() {
+      let q = FactoryBoy.create('question1');
+      let rsStart = q.TimesAnswered.RightSum;
+      let ans = { Value: 1 };
+      q.addAnswer(ans);
+      let rsAdd = q.TimesAnswered.RightSum;
+      chai.assert(rsAdd == rsStart + 1);
+      q.removeAnswer(ans);
+      let rsRemove = q.TimesAnswered.RightSum;
+      chai.assert(rsRemove == rsAdd - 1);
+    });
+
+    it('allAnsweredUsers returns at least one user that answered the question', function() {
+      let q = FactoryBoy.create('question1');
+      let u = FactoryBoy.create('user2');
+      let a = FactoryBoy.build('answer1');
+      //u.MyProfile.UserType.answerQuestion(q);
+      let myStub = sinon.stub(Meteor, 'userId').returns(u);
+      User.update(
+        { _id: u._id },
+        { $push: { 'MyProfile.UserType.AnsweredQuestions': a } }
+      );
+      myStub.restore();
+
+      let u2 = User.find({ _id: u._id }).fetch();
+      //console.log(u2[0].MyProfile.UserType.AnsweredQuestions[0].QuestionID);
+      chai.assert.equal(
+        u2[0].MyProfile.UserType.AnsweredQuestions[0].QuestionID,
+        q._id,
+        'Answer ID is not the same as Question ID'
+      );
+      let ansUser = q.allAnsweredUsers();
+      //console.log(ansUser.fetch());
+      chai.assert.equal(
+        ansUser.fetch()[0]._id,
+        u._id,
+        "question.allAnsweredUsers doesn't match the user ID"
+      );
+    });
+    it('reset() resets TimesAnswered and SumOfAnswers', function() {
+      let q = FactoryBoy.create('question1');
+      let ans = { Value: -2 };
+      q.addAnswer(ans);
+      let taStart = q.TimesAnswered.LeftSum;
+      let soaStart = q.SumOfAnswers.LeftSum;
+      chai.assert(
+        taStart == 1 && soaStart == -2,
+        'Error while adding an answer'
+      );
+      q.reset();
+      let taFinish = q.TimesAnswered.LeftSum;
+      let soaFinish = q.SumOfAnswers.LeftSum;
+      chai.assert(
+        taFinish == 0 && soaFinish == 0,
+        'Error while reseting answers'
+      );
+    });
+  });
+}
--- imports/api/users/users.tests.js
+++ imports/api/users/users.tests.js
@@ -5,174 +5,197 @@ import { Team } from '../teams/teams.js';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 //commented the above code line out because it was failing in the build production test.
 
-FactoryBoy.define("nonAdminUser1", User, {
-    _id: "1234567899912839",
-    services: {
-        password: {}
+FactoryBoy.define('nonAdminUser1', User, {
+  _id: '1234567899912839',
+  services: {
+    password: {}
+  },
+  username: 'bestTestUser',
+  emails: [],
+  slug: 'testUser@domain.com',
+  MyProfile: {
+    firstName: 'testUser',
+    lastName: 'test',
+    gender: true,
+    UserType: {
+      Personality: {},
+      AnsweredQuestions: []
     },
-    username: "bestTestUser",
-    emails: [],
-    slug: "testUser@domain.com",
-    MyProfile: {
-        firstName: "testUser",
-        lastName: "test",
-        gender: true,
-        UserType: {
-            Personality: {},
-            AnsweredQuestions: []
-        },
-        birthDate: new Date("December 17, 1995 03:24:00")
-    },
-    teams: [],
-    roles: {},
-    profile: {
-        first_name: "testUser",
-        last_name: "test",
-        gender: "male"
-    }
+    birthDate: new Date('December 17, 1995 03:24:00'),
+    technicalSkillsData: 'th3TestK3y'
+  },
+  teams: [],
+  roles: {},
+  profile: {
+    first_name: 'testUser',
+    last_name: 'test',
+    gender: 'male'
+  }
 });
 
-FactoryBoy.define("adminUser1", User, {
-    _id: "999",
-    services: {
-        password: {}
-    },
-    MyProfile: {
-        firstName: "adminUser",
-        lastName: "admin",
-        gender: true
-    },
-    roles: {
-        __global_roles__: ["admin"]
-    }
+FactoryBoy.define('adminUser1', User, {
+  _id: '999',
+  services: {
+    password: {}
+  },
+  MyProfile: {
+    firstName: 'adminUser',
+    lastName: 'admin',
+    gender: true,
+    technicalSkillsData: 'th3TestK3y'
+  },
+  roles: {
+    __global_roles__: ['admin']
+  }
 });
 
-FactoryBoy.define("TestTeam", Team, {
-    Name: "theRealTestTeam1",
-    Description: "team description",
-    CreatedBy: "The Man, The Myth, The Legend.",
-    Members: ["coolest member"]
+FactoryBoy.define('TestTeam', Team, {
+  Name: 'theRealTestTeam1',
+  Description: 'team description',
+  CreatedBy: 'The Man, The Myth, The Legend.',
+  Members: ['coolest member']
 });
 
 if (Meteor.isServer) {
-    describe('User', function () {
-
-        it('can create a new non admin user', function testCreateUser(done) {
-            resetDatabase()
-            FactoryBoy.create("nonAdminUser1", { _id: "1234567899912839" })
-            let dbLookupUser = User.findOne({ _id: "1234567899912839" })
-            chai.assert(dbLookupUser !== undefined, "Non Admin User was not created")
-            done()
-        })
-
-        it('can create a new admin user', function testCreateAdminUser(done) {
-            resetDatabase()
-            FactoryBoy.create("adminUser1", { _id: '999' })
-            let dbLookupUser = User.findOne({_id: "999"})
-            chai.assert(dbLookupUser !== undefined, "Admin User was not created")
-            done()
-        });
-
-        it('user full name is returned by full name method', function returnUserFullName(done) {
-            resetDatabase()
-            FactoryBoy.create("nonAdminUser1", { _id: "1234567899912839" })
-            let dbLookupUser = User.findOne({ _id: "1234567899912839" })
-            let firstName = dbLookupUser.MyProfile.firstName
-            let lastName = dbLookupUser.MyProfile.lastName
-            let userName = firstName + ' ' + lastName
-            let result = (dbLookupUser.fullName() === userName) ? true : false
-            chai.assert(result === true, 'user full name is not being returned by full name method')
-            done()
-        })
-
-        it('user can update their profile information', function testUpdateProfile(done) {
-            resetDatabase()
-            let nonAdminUser = FactoryBoy.create("nonAdminUser1", { _id: "1234567899912839" })
-            let myStub = sinon.stub(Meteor, "userId")
-            myStub.returns(nonAdminUser)
-            let getloggedInUser = () => User.findOne({ _id: Meteor.userId()._id }) 
-            let loggedInUser = getloggedInUser()
-            let uprofile = {
-                firstName: 'Charlie',
-                lastName: 'Testerino',
-            };
-            loggedInUser.profileUpdate(uprofile)
-            loggedInUser = getloggedInUser()
-            let result = ( (loggedInUser.MyProfile.firstName === 'Charlie') && (loggedInUser.MyProfile.lastName === 'Testerino') ) ? true : false 
-            chai.assert(result === true, 'User profile information did not update')
-            myStub.restore()
-            done()
-        })
-
-        it('user can be added to a team by the addTeam method', function testAddTeamMethod(done) {
-            resetDatabase()
-            FactoryBoy.create('TestTeam')
-            FactoryBoy.create("nonAdminUser1", { _id: "1234567899912839" })
-            let user = User.findOne({ _id: '1234567899912839'})
-            user.addTeam('theRealTestTeam1')
-            user = User.findOne({ _id: "1234567899912839"})
-            let result = (user.roles.theRealTestTeam1 !== undefined) ? true : false
-            chai.assert(result === true, 'User is not part of the test team')
-            done()
-        })
-
-        it('user can change name with changeName method', function testChangeNameMethod(done) {
-            resetDatabase()
-            FactoryBoy.create("nonAdminUser1", { _id: "1234567899912839" })
-            
-            let firstName = 'Charlie'
-            let lastName = 'Testerino'
-            let user = User.findOne({ _id: "1234567899912839" })
-            let changedFirstName, changedLastName, result
-            
-            user.changeName(firstName, lastName)
-            user = User.findOne({ _id: "1234567899912839" })
-            changedFirstName = user.MyProfile.firstName
-            changedLastName = user.MyProfile.lastName
-            result = ((firstName === changedFirstName) && (lastName === changedLastName)) ? true : false
-            chai.assert(result === true, 'User name is not changing using changeName method')
-            done()
-        })
-
-        it('user can add roles', function testAddRoleMethod(done) {
-            resetDatabase()
-            let adminUser = FactoryBoy.create("adminUser1", { _id: "999" })
-            let newRole, user, stub, result 
-            stub = sinon.stub(Meteor, "userId")
-            stub.returns(adminUser)
-            newRole = 'member'
-            user = User.findOne({ _id: "999" })
-            user.addRole(newRole)
-            user = User.findOne({ _id: "999" })
-            result = (user.roles.__global_roles__.indexOf(newRole) > -1) ? true : false 
-            chai.assert(result === true, 'User role was not added via addRole method')
-            stub.restore()
-            done()
-        })
-
-        it('user can remove roles', function testRemoveRoleMethod(done) {
-            resetDatabase()
-            let adminUser = FactoryBoy.create("adminUser1", { _id: "999" })
-            let newRole, user, stub, result
-
-            stub = sinon.stub(Meteor, "userId")
-            stub.returns(adminUser)
-            
-            newRole = 'member'
-
-            user = User.findOne({ _id: "999" })
-            user.addRole(newRole)
-            
-            user = User.findOne({ _id: "999" })
-            user.removeRole(newRole)
-            
-            user = User.findOne({ _id: "999" })
-
-            result = (user.roles.__global_roles__.indexOf(newRole) === -1) ? true : false
-            
-            chai.assert(result === true, 'User role was not removed via removeRole method')
-            stub.restore()
-            done()
-        })
+  describe('User', function() {
+    it('can create a new non admin user', function testCreateUser(done) {
+      resetDatabase();
+      FactoryBoy.create('nonAdminUser1', { _id: '1234567899912839' });
+      let dbLookupUser = User.findOne({ _id: '1234567899912839' });
+      chai.assert(dbLookupUser !== undefined, 'Non Admin User was not created');
+      done();
     });
+
+    it('can create a new admin user', function testCreateAdminUser(done) {
+      resetDatabase();
+      FactoryBoy.create('adminUser1', { _id: '999' });
+      let dbLookupUser = User.findOne({ _id: '999' });
+      chai.assert(dbLookupUser !== undefined, 'Admin User was not created');
+      done();
+    });
+
+    it('user full name is returned by full name method', function returnUserFullName(done) {
+      resetDatabase();
+      FactoryBoy.create('nonAdminUser1', { _id: '1234567899912839' });
+      let dbLookupUser = User.findOne({ _id: '1234567899912839' });
+      let firstName = dbLookupUser.MyProfile.firstName;
+      let lastName = dbLookupUser.MyProfile.lastName;
+      let userName = firstName + ' ' + lastName;
+      let result = dbLookupUser.fullName() === userName ? true : false;
+      chai.assert(
+        result === true,
+        'user full name is not being returned by full name method'
+      );
+      done();
+    });
+
+    it('user can update their profile information', function testUpdateProfile(done) {
+      resetDatabase();
+      let nonAdminUser = FactoryBoy.create('nonAdminUser1', {
+        _id: '1234567899912839'
+      });
+      let myStub = sinon.stub(Meteor, 'userId');
+      myStub.returns(nonAdminUser);
+      let getloggedInUser = () => User.findOne({ _id: Meteor.userId()._id });
+      let loggedInUser = getloggedInUser();
+      let uprofile = {
+        firstName: 'Charlie',
+        lastName: 'Testerino'
+      };
+      loggedInUser.profileUpdate(uprofile);
+      loggedInUser = getloggedInUser();
+      let result =
+        loggedInUser.MyProfile.firstName === 'Charlie' &&
+        loggedInUser.MyProfile.lastName === 'Testerino'
+          ? true
+          : false;
+      chai.assert(result === true, 'User profile information did not update');
+      myStub.restore();
+      done();
+    });
+
+    it('user can be added to a team by the addTeam method', function testAddTeamMethod(done) {
+      resetDatabase();
+      FactoryBoy.create('TestTeam');
+      FactoryBoy.create('nonAdminUser1', { _id: '1234567899912839' });
+      let user = User.findOne({ _id: '1234567899912839' });
+      user.addTeam('theRealTestTeam1');
+      user = User.findOne({ _id: '1234567899912839' });
+      let result = user.roles.theRealTestTeam1 !== undefined ? true : false;
+      chai.assert(result === true, 'User is not part of the test team');
+      done();
+    });
+
+    it('user can change name with changeName method', function testChangeNameMethod(done) {
+      resetDatabase();
+      FactoryBoy.create('nonAdminUser1', { _id: '1234567899912839' });
+
+      let firstName = 'Charlie';
+      let lastName = 'Testerino';
+      let user = User.findOne({ _id: '1234567899912839' });
+      let changedFirstName, changedLastName, result;
+
+      user.changeName(firstName, lastName);
+      user = User.findOne({ _id: '1234567899912839' });
+      changedFirstName = user.MyProfile.firstName;
+      changedLastName = user.MyProfile.lastName;
+      result =
+        firstName === changedFirstName && lastName === changedLastName
+          ? true
+          : false;
+      chai.assert(
+        result === true,
+        'User name is not changing using changeName method'
+      );
+      done();
+    });
+
+    it('user can add roles', function testAddRoleMethod(done) {
+      resetDatabase();
+      let adminUser = FactoryBoy.create('adminUser1', { _id: '999' });
+      let newRole, user, stub, result;
+      stub = sinon.stub(Meteor, 'userId');
+      stub.returns(adminUser);
+      newRole = 'member';
+      user = User.findOne({ _id: '999' });
+      user.addRole(newRole);
+      user = User.findOne({ _id: '999' });
+      result = user.roles.__global_roles__.indexOf(newRole) > -1 ? true : false;
+      chai.assert(
+        result === true,
+        'User role was not added via addRole method'
+      );
+      stub.restore();
+      done();
+    });
+
+    it('user can remove roles', function testRemoveRoleMethod(done) {
+      resetDatabase();
+      let adminUser = FactoryBoy.create('adminUser1', { _id: '999' });
+      let newRole, user, stub, result;
+
+      stub = sinon.stub(Meteor, 'userId');
+      stub.returns(adminUser);
+
+      newRole = 'member';
+
+      user = User.findOne({ _id: '999' });
+      user.addRole(newRole);
+
+      user = User.findOne({ _id: '999' });
+      user.removeRole(newRole);
+
+      user = User.findOne({ _id: '999' });
+
+      result =
+        user.roles.__global_roles__.indexOf(newRole) === -1 ? true : false;
+
+      chai.assert(
+        result === true,
+        'User role was not removed via removeRole method'
+      );
+      stub.restore();
+      done();
+    });
+  });
 }
