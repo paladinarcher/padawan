980237ac7f92efa4b19f56b9aafc873044b6b709
Carl Shurtliff <cphilshurtliff@gmail.com>
2019-05-02 15:12:14 -0600

    Added totals and averages to tsq results

--- imports/ui/pages/tsq/results/results.js
+++ imports/ui/pages/tsq/results/results.js
@@ -27,5 +27,57 @@ Template.tsq_results.helpers({
     },
     returnConfidenceStatement(level) {
         return confidenceStatments[level.hash.level.toString()]
+    },
+    familiarCount() {
+        familiar = 0;
+        keyInfo.get().skills.forEach((value, index) => {
+            console.log("value, index: ", value, index);
+            if (value.familiar === true) {
+                familiar += 1;
+            }
+        });
+        return familiar;
+    },
+    unfamiliarCount() {
+        unfamiliar = 0;
+        keyInfo.get().skills.forEach((value, index) => {
+            console.log("value, index: ", value.familiar, index);
+            if (value.familiar === false) {
+                unfamiliar += 1;
+            }
+        });
+        return unfamiliar;
+    },
+    familiarAverage() {
+        familiar = 0;
+        confidenceSum = 0
+        keyInfo.get().skills.forEach((value, index) => {
+            console.log("value, index: ", value, index);
+            if (value.familiar === true) {
+                familiar += 1;
+                confidenceSum += value.confidenceLevel;
+            }
+        });
+        if (familiar > 0) {
+            return confidenceSum / familiar;
+        } else {
+            return "No Familiar Technology"
+        }
+    },
+    unfamiliarAverage() {
+        unfamiliar = 0;
+        confidenceSum = 0
+        keyInfo.get().skills.forEach((value, index) => {
+            console.log("value, index: ", value, index);
+            if (value.familiar === false) {
+                unfamiliar += 1;
+                confidenceSum += value.confidenceLevel;
+            }
+        });
+        if (unfamiliar > 0) {
+            return confidenceSum / unfamiliar;
+        } else {
+            return "No Unfamiliar Technology"
+        }
     }
 })
