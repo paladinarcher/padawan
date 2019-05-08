25d5cd5a42f7db4d8d904a4cab441863f58703a2
Carl Shurtliff <cphilshurtliff@gmail.com>
2019-05-08 11:43:11 -0600

    update custom mbti reports button and qnaireMbti results link added

--- imports/api/reports/customReports.js
+++ imports/api/reports/customReports.js
@@ -41,6 +41,48 @@ class mbtiReport  {
         })
     }
 
+}
+
+class qnaireMbtiReport  {
+
+    constructor() {
+        this.mbtiReport = new Report()
+        this.userData = User.find().fetch()
+        this.mbtiReport.reportData = { all: [] }
+        this.mbti = Reports.findOne({title: 'qnaireMbti'})
+    }
+
+    createQnaireMBTIReport() {
+        this.userData.forEach(u => {
+            this.mbtiReport.reportData.all.push({
+                userName: u.MyProfile.firstName + ' ' + u.MyProfile.lastName,
+                personality: u.MyProfile.UserType.Personality,
+            })
+        })
+        return this.mbtiReport
+    }
+
+    addQnaireMBTIReport () {
+        let newMBTIReport = new Reports({
+            title: 'qnaireMbti',
+            description: 'qnaire MBTI Results for all team members',
+            dateCreated: new Date(),
+            custom: true,
+            data: this.createQnaireMBTIReport()
+        })
+        newMBTIReport.save();
+    }
+
+    updateMBTIReport () {
+        Reports.update(this.mbti._id, {
+            title: 'qnaireMbti',
+            description: 'qnaire MBTI Results for all team members',
+            dateCreated: new Date(),
+            custom: true,
+            data: this.createQnaireMBTIReport()
+        })
+    }
+
 } 
 
-export { mbtiReport }
+export { mbtiReport, qnaireMbtiReport }
--- imports/api/reports/methods.js
+++ imports/api/reports/methods.js
@@ -1,7 +1,12 @@
 import { Reports, Report } from './reports.js'
-import { mbtiReport } from './customReports.js';
+import { mbtiReport, qnaireMbtiReport } from './customReports.js';
 
 Meteor.methods({
+    // MBTI report
+    'addMBTIReport': function () { 
+        let mbti = new mbtiReport();
+        mbti.addMBTIReport();
+    },
     'updateMBTIReport': function () { 
         let currentMBTI = Reports.findOne({title: 'mbti'})
         let newMBTI = new mbtiReport()
@@ -12,5 +17,21 @@ Meteor.methods({
             custom: true,
             data: newMBTI.createMBTIReport()
         })
-    }
+    },
+    // Qnaire MBTI report
+    'updateQnaireMBTIReport': function () { 
+        let currentMBTI = Reports.findOne({title: 'mbti'})
+        let newMBTI = new mbtiReport()
+        return Reports.update(currentMBTI._id, {
+            title: 'mbti',
+            description: 'MBTI Results for all team members',
+            dateCreated: new Date(),
+            custom: true,
+            data: newMBTI.createMBTIReport()
+        })
+    },
+    'addQnaireMBTIReport': function () { 
+        let qnaireMbti = new qnaireMbtiReport();
+        qnaireMbti.addQnaireMBTIReport();
+    },
 })
