6b1350e3b079a94bf8e8a2402a81b8f615219993
Keola <keola.goodman@gmail.com>
2019-06-06 09:46:06 -0600

    small updates to report

--- imports/ui/pages/admin_reports/opposites_report/opposites.js
+++ imports/ui/pages/admin_reports/opposites_report/opposites.js
@@ -5,34 +5,14 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import './opposites.html';
 
-var minQuestionsAnswered = 72;
+// var minQuestionsAnswered = 72;
 
-function htmlToElement(html) {
-    var template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    template.innerHTML = html;
-    return template.content.firstChild;
-}
-
-function hacks(){
-    let users = Meteor.users.find({}).fetch();
-        users = users.filter((user) => {
-            return user.MyProfile.UserType.AnsweredQuestions.length > 71;
-        });
-        let responders = [];
-            if (users.length)  {
-                users.forEach(function (user) {
-                    responders = responders.concat(user.MyProfile);
-                }); 
-            };
-        this.responders = responders;
-        console.log('responders: ', responders);
-        
-        return responders;
-}
-
-
-
+// function htmlToElement(html) {
+//     var template = document.createElement('template');
+//     html = html.trim(); // Never return a text node of whitespace as the result
+//     template.innerHTML = html;
+//     return template.content.firstChild;
+// }
 
 Template.opposite_responses.onCreated(function () {
     if (this.data.userId) {
@@ -48,45 +28,45 @@ Template.opposite_responses.onCreated(function () {
     }
     this.subscribe('userList');
 
-    this.subscribe('questions.bycategory', 0, {
-        onStop: function () {
-            console.log("QQ Subscription stopped! ", arguments, this);
-        }, onReady: function () {
-            console.log("QQ Subscription ready! ", arguments, this);
-        },
-        sort: {createdAt: -1}
-    });
+//     this.subscribe('questions.bycategory', 0, {
+//         onStop: function () {
+//             console.log("QQ Subscription stopped! ", arguments, this);
+//         }, onReady: function () {
+//             console.log("QQ Subscription ready! ", arguments, this);
+//         },
+//         sort: {createdAt: -1}
+//     });
 
-    this.subscribe('questions.bycategory', 1, {
-        onStop: function () {
-            console.log("QQ Subscription stopped! ", arguments, this);
-        }, onReady: function () {
-            console.log("QQ Subscription ready! ", arguments, this);
-        },
-        sort: {createdAt: -1}
-    });
+//     this.subscribe('questions.bycategory', 1, {
+//         onStop: function () {
+//             console.log("QQ Subscription stopped! ", arguments, this);
+//         }, onReady: function () {
+//             console.log("QQ Subscription ready! ", arguments, this);
+//         },
+//         sort: {createdAt: -1}
+//     });
 
-    this.subscribe('questions.bycategory', 2, {
-        onStop: function () {
-            console.log("QQ Subscription stopped! ", arguments, this);
-        }, onReady: function () {
-            console.log("QQ Subscription ready! ", arguments, this);
-        },
-        sort: {createdAt: -1}
-    });
+//     this.subscribe('questions.bycategory', 2, {
+//         onStop: function () {
+//             console.log("QQ Subscription stopped! ", arguments, this);
+//         }, onReady: function () {
+//             console.log("QQ Subscription ready! ", arguments, this);
+//         },
+//         sort: {createdAt: -1}
+//     });
 
-    this.subscribe('questions.bycategory', 3, {
-        onStop: function () {
-            console.log("QQ Subscription stopped! ", arguments, this);
-        }, onReady: function () {
-            console.log("QQ Subscription ready! ", arguments, this);
-        },
-        sort: {createdAt: -1}
-    });
+//     this.subscribe('questions.bycategory', 3, {
+//         onStop: function () {
+//             console.log("QQ Subscription stopped! ", arguments, this);
+//         }, onReady: function () {
+//             console.log("QQ Subscription ready! ", arguments, this);
+//         },
+//         sort: {createdAt: -1}
+//     });
 
-    this.autorun( () => {
+//     this.autorun( () => {
         
-    });
+//     });
 });
 
 
@@ -175,10 +155,12 @@ Template.opposite_responses.helpers({
             // console.log('response4: ', responses);
             let id = response.QuestionID;
             //let question = Meteor.questions.findOne({ _id: id }).LeftText;
+            
             let value =  response.Value;
             let info = 'Question ID: ' + id + ' ' + 'Value: ' + value;     
             return info;
         });
+        console.log('qs: ', Meteor);
         return value;
     },
     ansNS(u) {
@@ -268,95 +250,8 @@ Template.opposite_responses.helpers({
         });
         return value;
     },
-    // answered(ut, item) {
-    //     //hacks();
-    //     let response = ut.AnsweredQuestions; 
-    //     let answers = [];
-        
-    //     response.forEach(function() {
-    //         answers = answers.concat(ut.AnsweredQuestions);
-    //     });
-        
-        
-    //     this.answers = answers;
-    //     console.log('answer: ', answers);
-        
-    //     let value = answers.map(function(answer){
-    //         return answer.Value;
-    //     })
-
-    //     return value;
-    // },
-
-    user() {
-        return User.findOne({_id:Template.instance().userId});
-    },
-    isMinMet() {
-        let u = User.findOne({_id:Template.instance().userId});
-        if (!u) return false;
-        if (u.MyProfile.UserType.AnsweredQuestions.length >= minQuestionsAnswered) {
-            return true;
-        } else {
-            return false;
-        }
-    },
-    opacityByCategory(category, userObj) {
-        if (typeof userObj === "undefined") return false;
-        var value = userObj.MyProfile.UserType.Personality[userObj.MyProfile.UserType.Personality.getIdentifierById(category)];
-        return (Math.abs(value.Value) * 2) / 100;
-    },
-    letterByCategory(category, userObj) {
-        if (typeof userObj === "undefined") return false;
-        var identifier = userObj.MyProfile.UserType.Personality.getIdentifierById(category);
-        var value = userObj.MyProfile.UserType.Personality[identifier].Value;
-        if (userObj.MyProfile.UserType.AnsweredQuestions.length >= minQuestionsAnswered) {
-            return (value === 0 ? "?" : (value < 0 ? identifier.slice(0,1) : identifier.slice(1,2)));
-        } else {
-            return "?";
-        }
-    },
    
-    results(category) {
-        let identifier = UserType.Personality.getIdentifierById(
-          category
-        );
-            console.log('result identify: ', identifier );
-            
-        let identifierValue =
-          UserType.Personality[identifier].Value;
-
-        let percentageValue =
-          UserType.Personality[
-            UserType.Personality.getIdentifierById(category)
-          ];
     
-        let percentage = Math.ceil(Math.abs(percentageValue.Value));
-    
-        if (identifierValue) {
-            console.log('percentage: ', percentage);
-            
-          return 50 + percentage;
-        }
-    },
-    // results(category, userObj) {
-    //     let identifier = userObj.MyProfile.UserType.Personality.getIdentifierById(
-    //       category
-    //     );
-
-    //     let identifierValue =
-    //       userObj.MyProfile.UserType.Personality[identifier].Value;
-
-    //     let percentageValue =
-    //       userObj.MyProfile.UserType.Personality[
-    //         userObj.MyProfile.UserType.Personality.getIdentifierById(category)
-    //       ];
-    
-    //     let percentage = Math.ceil(Math.abs(percentageValue.Value));
-    
-    //     if (identifierValue) {
-    //       return 50 + percentage;
-    //     }
-    // },
     answers() {
         // const u = User.findOne({_id:Template.instance().userId});
         const users = Meteor.users.find({}).fetch();
