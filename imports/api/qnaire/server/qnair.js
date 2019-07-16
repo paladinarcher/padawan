aac070cf0b52516e2278cbe435b4019fca0cc419
Jay Ward <killinggameinstructor@gmail.com>
2019-07-16 07:33:47 -0600

    making timeout configurable by environment

--- imports/api/qnaire/server/qnair.js
+++ imports/api/qnaire/server/qnair.js
@@ -1,13 +1,14 @@
 import { HelperPages } from '../../help/helperPages.js';
 import { Qnaire } from '../qnaire.js';
 
+const timeoutInMinutes = Meteor.settings.public.Pages.Base.TimeoutInMinutes;
 
 Qnaire.extend({
   meteorMethods: {
     getIntroHTML() {
       var date = new Date();
       //date.setDate(date.getDate() - 15);
-      date.setMinutes(date.getMinutes() - 1);
+      date.setMinutes(date.getMinutes() - timeoutInMinutes);
       if(this.introCache != "" && date < this.lastCheckedIntro) { return this.introCache; }
       if(this.introSlug == "") { 
         this.introSlug = this.title.toLowerCase().replace(/[^\w]+/g, "-")+"-introduction";
@@ -21,7 +22,7 @@ Qnaire.extend({
     getInstructionHTML() {
       var date = new Date();
       //date.setDate(date.getDate() - 15);
-      date.setMinutes(date.getMinutes() - 1);
+      date.setMinutes(date.getMinutes() - timeoutInMinutes);
       if(date < this.lastCheckedInstruction) { return this.instructionCache; }
       if(this.instructionSlug == "") { 
         this.instructionSlug = this.title.toLowerCase().replace(/[^\w]+/g, "-")+"-instructions";
