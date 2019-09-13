61863e914da48ec75f73006637a255686808a56e
Jay Ward <jay@paladinarcher.com>
2019-09-13 15:55:20 -0600

    consolidating some of the graphing functionality as well as adding queues

--- imports/api/queue/queue.js
+++ imports/api/queue/queue.js
@@ -1,8 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+//import Bull from 'bull';
+var Bull = require('bull');
 
-var Queue = require('bull');
-
-var UserActivitiesQueue = new Queue('User Activities Processing', {
+const UserActivitiesQueue = new Bull('User Activities Processing', {
   limiter: Meteor.settings.private.Queue.Limiter,
   redis: Meteor.settings.private.Queue.Redis
 });
