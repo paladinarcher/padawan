7120d81b0f106821a797c84cc9e97f14f4670904
Carl Shurtliff <cphilshurtliff@gmail.com>
2019-09-27 10:30:44 -0600

    this should fix the submit all btn bug

--- imports/ui/components/questions/questions.js
+++ imports/ui/components/questions/questions.js
@@ -6,7 +6,7 @@ import './questions.html';
 
 var minQuestionsAnswered = Question.MIN_ANSWERED;
 var stoppedList = [];
-var submitAllCLicked = false;
+let submitAllCLicked = false;
 
 Template.questions.onCreated(function () {
     if (this.data.userId) {
@@ -224,36 +224,34 @@ Template.questions.events({
     // submit all answers
     'click button#submitAll'(event, instance){
         event.preventDefault();
-        setTimeout(function() {
-            console.log('waiting');
-            // alert("waiting");
+        console.log('waiting');
+        // alert("waiting");
 
-            if (!submitAllCLicked) { // this fixes the bug where double clicking submit twice as many answers.
-                // submitAllClicked = true;
-                const ans = $('div.answer-question');
-                ans.each(function(){    
-                    const val = {
-                        questionId: $(this).data('id'),
-                        value: $(this).data('value'),
-                        isReversed: !!$(this).data('reversed')
-                    }
-                    Meteor.call('question.answer', val.questionId, val.value, val.isReversed, (error) => {
-                        if (error) {
-                            console.log("EEEEEERRRORRRRR: ", error);
-                        // } else {
-                        //     $(this).remove();
-                        //     if($('div.answer-question').length < 1) {
-                        //         Session.set('refreshQuestions', Math.random());
-                        //     }
-                        }
-                    });
-                })
-                ans.remove();
-                if($('div.answer-question').length < 1) {
-                    Session.set('refreshQuestions', Math.random());
+        if (!submitAllCLicked) { // this fixes the bug where double clicking submit twice as many answers.
+            submitAllClicked = true;
+            const ans = $('div.answer-question');
+            ans.each(function(){    
+                const val = {
+                    questionId: $(this).data('id'),
+                    value: $(this).data('value'),
+                    isReversed: !!$(this).data('reversed')
                 }
+                Meteor.call('question.answer', val.questionId, val.value, val.isReversed, (error) => {
+                    if (error) {
+                        console.log("EEEEEERRRORRRRR: ", error);
+                    // } else {
+                    //     $(this).remove();
+                    //     if($('div.answer-question').length < 1) {
+                    //         Session.set('refreshQuestions', Math.random());
+                    //     }
+                    }
+                });
+            })
+            ans.remove();
+            if($('div.answer-question').length < 1) {
+                Session.set('refreshQuestions', Math.random());
             }
-        }, 4000);
+        }
     },
     'click button.btn-back-intro'(event, instance) {
       var lvl = instance.view.template.__helpers[" introLevel"](instance._helpLevel.get() + 1);
@@ -280,6 +278,7 @@ Template.question.helpers({
 });
 Template.question.onRendered(function() {
     console.log("onRendered", this);
+    submitAllCLicked = false;
     //let hidebtn = $('button.answer-button');
     let updateValue = function(elem, value) {
         let parent = $(elem).data('value', value);
