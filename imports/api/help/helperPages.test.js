ce2763f9fcea6c61b61e9d3f75a763dd05ddf5b9
Carl Shurtliff <cphilshurtliff@gmail.com>
2019-10-01 11:37:42 -0600

    finished HelperPages mocha test with 100% istanbul coverage

--- imports/api/help/helperPages.test.js
+++ imports/api/help/helperPages.test.js
@@ -31,10 +31,10 @@ if (Meteor.isServer) {
             let devRe = new RegExp(helpBaseURL);
             // devRe = /supercalifrag/; // remove this
             chai.assert.isTrue(devRe.test(myUrl), 'getPageBySlug does not add the base url');
-            console.log('slug url: ', myUrl);
+            // console.log('slug url: ', myUrl);
         });
         //getPageURL
-        it('getPageByURL', function () {
+        it('getPageByURL returns url with correct password', function () {
             // console.log('pass in test: ', Meteor.settings.public.Pages);
             let myUrl = HelperPages.getPageURL();
             // console.log('myByUrl: ', myUrl);
@@ -61,7 +61,7 @@ if (Meteor.isServer) {
             // newPassRe = /dev/; // remove this
             chai.assert.isFalse(newPassRe.test(myUrl), '&password= should not show up');
 
-            Meteor.settings.public.Pages.Base.Password = helpPassword; //put password back
+            Meteor.settings.public.Pages.Base.Password = helpPassword; //put password back where you found it
         });
         //getPageObject
         it('getPageObject returns an object', function (done) {
@@ -74,10 +74,33 @@ if (Meteor.isServer) {
             done();
         })
         //getPageObjectBySlug
-        it('getPageObjectBySlug', function () {
-            console.log('todo');
+        it('getPageObjectBySlug returns the terms of service', function (done) {
+            this.timeout(10000); // giving extra time so Jenkins is less likely to crash
+            let myUrl = HelperPages.getPageObjectBySlug('terms-of-service');
+            // console.log('myUrl: ', myUrl);
+            // console.log('myUrl title: ', myUrl.title);
+            // console.log('myUrl title rendered: ', myUrl.title.rendered);
+            chai.assert.strictEqual(myUrl.title.rendered, 'Terms of Service', 'getPageObjectBySlug should have returned the terms of service');
+            let slugRe = /terms-of-service/;
+
+            let myStub = sinon.stub(HelperPages, "getPageObject");
+            myStub.returns([]);
+            myUrl = HelperPages.getPageObjectBySlug('terms-of-service');
+            // console.log('myUrl: ', myUrl);
+            chai.assert.strictEqual(myUrl.content.rendered, '', 'getPageObjectBySlug should have returned rendered as an empty string');
+            myStub.restore();
+            done();
         })
         //getPageContentBySlug
+        it('getPageContentBySlug ruturns terms of service rendered content', function (done) {
+            this.timeout(10000); // giving extra time so Jenkins is less likely to crash
+            let myRendered = HelperPages.getPageContentBySlug('terms-of-service');
+            // console.log('myRendered: ', myRendered);
+            let renderedRe = /Terms\sof\sService\sAgreement/;
+            // console.log('renderedRe.test(myRendered): ', renderedRe.test(myRendered));
+            chai.assert.isTrue(renderedRe.test(myRendered), 'getPageContentBySlug did not return the Terms of Service Agreement content');
+            done();
+        })
     });
 
 }
