6aec335247c7d8a2ee662e606a1eebcd2e09a6cb
Carl Shurtliff <cphilshurtliff@gmail.com>
2019-10-15 12:44:35 -0600

    Google Analytics are now inserted into head based on environment (development or production)

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -4,3 +4,27 @@ import '../both/defaults.js';
 import './defaults.js';
 import './fixtures.js';
 import './register-api.js';
+
+// inserts google analytics based on if server is development or production
+WebAppInternals.registerBoilerplateDataCallback('someKey', (request, data, arch) => {
+    if(Meteor.isDevelopment) {
+        data.head = data.head + `
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-139764993-2');
+        </script>`;
+    }
+    if(Meteor.isProduction) {
+        data.head = data.head + `
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-139764993-1');
+        </script>`;
+    }
+});
