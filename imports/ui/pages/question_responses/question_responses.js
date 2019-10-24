deb5f3748e1989249d636a8a8bf471699a3feb7a
baustin <bryant.austin@va.gov>
2019-10-24 12:53:10 -0600

    code clean up

--- imports/ui/pages/question_responses/question_responses.js
+++ imports/ui/pages/question_responses/question_responses.js
@@ -19,15 +19,6 @@ console.log("QuestionNumber =======    " + questionNumber);
                 }
             }
         });
-/*
-        this.getQuestionNumber = () => {
-            return this.data.questionNumberDisplay;
-        };
-        this.getQuestionAnsweredText = () => {
-            return this.data.questionAnsweredText;
-        };
-
- */
         this.subscription = this.subscribe('questions.questionList', {
             onReady: () => (Meteor.isDevelopment) ? console.log({ subName: 'questions', readyStatus: true, arguments, THIS: this}) : null,
             onError: () => (Meteor.isDevelopment) ? console.log({ subName: 'questions', readyStatus: false, arguments, THIS: this}) : null,
@@ -52,165 +43,74 @@ console.log("QuestionNumber =======    " + questionNumber);
            });
             return aqValue;
         },
-        getLeftText(){
+        getLeftRightText(whichSide){
             let questionFound = Question.findOne({Text: Template.instance().data.questionAnsweredText});
-            return questionFound.LeftText;
+            if("L" === whichSide) {
+                return questionFound.LeftText;
+            }else{
+                return questionFound.RightText;
+            }
         },
-        getRightText(){
-            let questionFound = Question.findOne({Text: Template.instance().data.questionAnsweredText});
-            return questionFound.RightText;
-        },
-        getI(userId) {
-            var aqValue = 0;
-            var count = 0;
+        getTraitValue(whichTrait, userId){
+            let aqValue = 0;
+            let count = 0;
             let thisUser = User.findOne({_id: userId});
-            thisUser.MyProfile.UserType.AnsweredQuestions.forEach(aq => {
-                if (aq.Categories[0] === 0) {
-                    if (aq.Value < 0) {
-                        aqValue += aq.Value;
-                        count++;
-                    }
-                }
-            });
-            if (count > 0) {
-                aqValue /= count;
-                aqValue *= -1;
-            }else{
-                aqValue = 0;
+            let cat = 0;
+            let direction = 'L';
+            switch(whichTrait){
+                case 'E':
+                    cat = 0;
+                    direction = "R"
+                    break;
+                case 'I':
+                    cat = 0;
+                    direction = "L"
+                    break;
+                case 'S':
+                    cat = 1;
+                    direction = "R"
+                    break;
+                case 'N':
+                    cat = 1;
+                    direction = "L"
+                    break;
+                case 'T':
+                    cat = 2;
+                    direction = "L"
+                    break;
+                case 'F':
+                    cat = 2;
+                    direction = "R"
+                    break;
+                case 'J':
+                    cat = 3;
+                    direction = "L"
+                    break;
+                case 'P':
+                    cat = 3;
+                    direction = "R"
+                    break;
             }
-            return aqValue.toFixed(2);
-        },
-        getE(userId){
-            var aqValue = 0;
-            var count = 0;
-            let thisUser = User.findOne({ _id: userId});
             thisUser.MyProfile.UserType.AnsweredQuestions.forEach(aq => {
-                if(aq.Categories[0] === 0){
-                    if(aq.Value > 0){
-                        aqValue += aq.Value;
-                        count++;
+                if (aq.Categories[0] === cat) {
+                    if(direction === "L"){
+                        if (aq.Value < 0) {
+                            aqValue += aq.Value;
+                            count++;
+                        }
+                    }else{
+                        if (aq.Value > 0) {
+                            aqValue += aq.Value;
+                            count++;
+                        }
                     }
                 }
             });
             if (count > 0) {
                 aqValue /= count;
-            }else{
-                aqValue = 0;
-            }
-            return aqValue.toFixed(2);
-        },
-        getN(userId){
-            var aqValue = 0;
-            var count = 0;
-            let thisUser = User.findOne({ _id: userId});
-            thisUser.MyProfile.UserType.AnsweredQuestions.forEach(aq => {
-                if(aq.Categories[0] === 1){
-                    if(aq.Value < 0){
-                        aqValue += aq.Value;
-                        count++;
-                    }
+                if(aqValue < 0){
+                    aqValue *= -1;
                 }
-            });
-            if (count > 0) {
-                aqValue /= count;
-                aqValue *= -1;
-            }else{
-                aqValue = 0;
-            }
-            return aqValue.toFixed(2);
-        },
-        getS(userId){
-            var aqValue = 0;
-            var count;
-            let thisUser = User.findOne({ _id: userId});
-            thisUser.MyProfile.UserType.AnsweredQuestions.forEach(aq => {
-                if(aq.Categories[0] === 1){
-                    if(aq.Value > 0){
-                        aqValue += aq.Value;
-                        count++;
-                    }
-                }
-            });
-            if (count > 0) {
-                aqValue /= count;
-            }else{
-                aqValue = 0;
-            }
-            return aqValue.toFixed(2);
-        },
-        getT(userId) {
-            var aqValue = 0;
-            var count = 0;
-            let thisUser = User.findOne({_id: userId});
-            thisUser.MyProfile.UserType.AnsweredQuestions.forEach(aq => {
-                if (aq.Categories[0] === 2) {
-                    if (aq.Value < 0) {
-                        aqValue += aq.Value;
-                        count++;
-                    }
-                }
-            });
-            if (count > 0) {
-                aqValue /= count;
-                aqValue *= -1;
-            }else{
-                aqValue = 0;
-            }
-            return aqValue.toFixed(2);
-        },
-        getF(userId){
-            var aqValue = 0;
-            var count = 0;
-            let thisUser = User.findOne({ _id: userId});
-            thisUser.MyProfile.UserType.AnsweredQuestions.forEach(aq => {
-                if(aq.Categories[0] === 2){
-                    if(aq.Value > 0){
-                        aqValue += aq.Value;
-                        count++;
-                    }
-                }
-            });
-            if (count > 0) {
-                aqValue /= count;
-            }else{
-                aqValue = 0;
-            }
-            return aqValue.toFixed(2);
-        },
-        getJ(userId){
-            var aqValue = 0;
-            var count = 0;
-            let thisUser = User.findOne({ _id: userId});
-            thisUser.MyProfile.UserType.AnsweredQuestions.forEach(aq => {
-                if(aq.Categories[0] === 3){
-                    if(aq.Value < 0){
-                        aqValue += aq.Value;
-                        count++;
-                    }
-                }
-            });
-            if (count > 0) {
-                aqValue /= count;
-                aqValue *= -1;
-            }else{
-                aqValue = 0;
-            }
-            return aqValue.toFixed(2);
-        },
-        getP(userId){
-            var aqValue = 0;
-            var count;
-            let thisUser = User.findOne({ _id: userId});
-            thisUser.MyProfile.UserType.AnsweredQuestions.forEach(aq => {
-                if(aq.Categories[0] === 3){
-                    if(aq.Value > 0){
-                        aqValue += aq.Value;
-                        count++;
-                    }
-                }
-            });
-            if (count > 0) {
-                aqValue /= count;
             }else{
                 aqValue = 0;
             }
