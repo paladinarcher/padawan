ee3bcd56444130734e30a94bef3be11bf78313cb
Carl Shurtliff <cphilshurtliff@gmail.com>
2019-11-04 12:20:06 -0700

    cleaned up comments

--- imports/startup/both/at_config.tests.js
+++ imports/startup/both/at_config.tests.js
@@ -4,7 +4,7 @@
 import { myPostLogout, mySubmitFunc, myPreSubmitFunc, myPostSubmitFunc, accessCodeFunc } from '/imports/startup/both/at_config.js';
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
-import { chai, expect } from 'meteor/practicalmeteor:chai';
+import { chai } from 'meteor/practicalmeteor:chai';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 import { Team } from '/imports/api/teams/teams.js';
 
@@ -28,42 +28,9 @@ FactoryBoy.define('myTeam', Team, {
 });
 
 if (Meteor.isServer) {
-    describe.only('startup/both/at_config', function () {
-        beforeEach(function () {
+    describe('startup/both/at_config', function () {
+        it('at_config should pass Istanbul coverage', function () {
             resetDatabase();
-        });
-
-        // it('myPostLogout function test', function () {
-        //     console.log('todo');
-        //     // Meteor.call('users.insert', user);
-        //     // Accounts.createUser(user);
-
-        //     // FactoryBoy.create('myTeam');
-        //     // let t = Team.findOne({ Name: Team.Default.Name });
-        //     // let uId = Accounts.createUser({
-        //     //     username: 'flyUserName',
-        //     //     email: 'FlyUserNam@mydomain.com',
-        //     //     password: 'password',
-        //     //     profile: {
-        //     //         first_name: 'Bob',
-        //     //         last_name: 'Thompson',
-        //     //         gender: 'male'
-        //     //     }
-        //     // });
-        //     // console.log('uId: ', uId);
-        //     // let u = Meteor.users.findOne({ _id: uId });
-        //     // console.log('u: ', u);
-
-        //     // myPostLogout();
-        //     // let retVal = Accounts.validateNewUser();
-        //     // let retVal = Accounts.validateLoginAttempt();
-        //     // console.log('retVal: ', retVal);
-        // });
-        it('test2 myPostLogout function test', function () {
-            console.log('todo');
-            let stubLog = sinon.stub(console, 'log').returns('dont console log');
-            // Meteor.call('users.insert', user);
-            // Accounts.createUser(user);
             let myTeam = FactoryBoy.create('myTeam');
             function conso() {
                 console.log('fake function called');
@@ -72,16 +39,6 @@ if (Meteor.isServer) {
                 eval('myPostSubmitFunc();');
                 return myTeam;
             }
-            // let postSubmitStub = sinon.stub(Team, 'findOne', conso);
-            // let postSubmitStub = sinon.stub(t, 'addUsers', conso);
-            // let postSubmitStub = sinon.stub(Team, 'findOne');
-            // postSubmitStub.callsFake(conso);
-            // postSubmitStub.callsFake(function() {
-            //     console.log('in fake function');
-            // });
-            // postSubmitStub.returns('hi');
-            // postSubmitStub.returns(conso.call());
-
             let t = Team.findOne({ Name: Team.Default.Name });
             let uId = Accounts.createUser({
                 username: 'flyUserName',
@@ -93,63 +50,17 @@ if (Meteor.isServer) {
                     gender: 'male'
                 }
             });
-            // console.log('uId: ', uId);
             let u = Meteor.users.findOne({ _id: uId });
-            
+            chai.assert.strictEqual('flyUserName', u.username, 'The users username should be flyUserName');
             myPostLogout();
-            // let subVal = 'signIn';
-            // let sesSpy = sinon.spy(Session, 'set');
-            // let sesStub = sinon.stub(Session, 'set');
-            // sesStub.returns('test');
-
-            // mySubmitFunc(false, 'signIn');
             mySubmitFunc(false, 'signUp');
-
-            // sesStub.restore();
-            // myPreSubmitFunc()
-
             myPostSubmitFunc(uId, 'sample info');
-
-            // console.log('sesSpy', sesSpy);
-            // chai.assert.strictEqual()
-            // console.log('Session.get("newLogin")', Session.get("newLogin"));
-            // console.log('subVal: ', subVal);
-
-            // Meteor.logout(function(err) {
-            //     console.log(err);
-            // });
-
-            // myPostLogout();
-            // let retVal = Accounts.validateNewUser();
-            // let retVal = Accounts.validateLoginAttempt();
-            // console.log('retVal: ', retVal);
-
-            // console.log('AccountsTemplates: ', AccountsTemplates);
-            // console.log('AccountsTemplates: ', AccountsTemplates);
-            // let stubClient = sinon.stub(Meteor, 'isClient');
-            // stubClient.get(() => { return true; });
-            // let stubClient = sinon.createSandbox();
-            // stubClient.stub(Meteor, 'isClient').value(true);
-            // let stubClient = sinon.stub(Meteor);
-            // stubClient.returns(true);
             accessCodeFunc('imaPADLo');
             accessCodeFunc('nothere');
-            // stubClient.restore();
+            let stubLog = sinon.stub(console, 'log').returns('dont console log');
             firstNameFunc('first');
             lastNameFunc('last');
             stubLog.restore();
         });
     });
 }
-// myPostLogout
-// test that myPostLogout sends FlowRouter to the signin page
-// mySubmitFunc
-// test signIn and signUp
-// myPreSubmitFunc
-// myPostSubmitFunc
-// addFields
-// test each func: function(value) code
-// onCreateUser 
-// test pretty much everything
-// validateNewUser
-// validateLoginAttempt
