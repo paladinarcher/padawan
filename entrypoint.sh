736d36255557d4f5c4589ebf6c18c50863b1d363
Carl Shurtliff <cphilshurtliff@gmail.com>
2020-01-22 10:50:55 -0700

    testing 16687

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 #/usr/local/bin/meteor update
 /usr/local/bin/meteor --allow-superuser --settings /app/settings.staging.json
-
-/usr/local/bin/meteor add lmieulet:meteor-coverage --allow-superuser meteortesting:mocha
-/usr/local/bin/meteor npm install --save-dev babel-plugin-istanbul
